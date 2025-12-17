4746a7cee6d18f8acb1176122ea42bea02ba1331
浑水摸鱼 <1813934922@qq.com>
2025-12-17 19:57:03 +0800

    增加了按键控制

---
+++ Core/Inc/key.h
@@ -0,0 +1,10 @@
+//
+// Created by 1234455 on 2025/12/17.
+//
+
+#ifndef PROJECT_KEY_H
+#define PROJECT_KEY_H
+#include <main.h>
+uint8_t Key_GetNum();
+
+#endif //PROJECT_KEY_H
---
+++ Core/Src/key.c
@@ -0,0 +1,23 @@
+//
+// Created by 1234455 on 2025/12/17.
+//
+#include <main.h>
+#include <gpio.h>
+
+#define Key_Pin GPIO_PIN_15
+
+uint8_t Key_GetNum()
+{
+    uint8_t Key_Num=0;
+    //static uint8_t Key_Num1=0;
+    if (HAL_GPIO_ReadPin(GPIOA,Key_Pin)==0)
+    {
+        HAL_Delay(20);
+        while(HAL_GPIO_ReadPin(GPIOA,Key_Pin)==0);
+        HAL_Delay(20);
+        Key_Num=1;
+        //Key_Num1=(Key_Num1+1)%2;                      //提供一个可开关的按键操作
+    }
+
+    return Key_Num;
+}
