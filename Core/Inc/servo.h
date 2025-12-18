f6dbc9e2c974d6d003706a40c67bd46b7b4df41e
浑水摸鱼 <1813934922@qq.com>
2025-12-18 10:27:54 +0800

    增加了舵机驱动

--- Core/Inc/servo.h
+++ Core/Inc/servo.h
@@ -5,4 +5,6 @@
 #ifndef PROJECT_SERVO_H
 #define PROJECT_SERVO_H
 #include <main.h>
+void Servo_Init();
+void Servo_Set_angle(const float angle);
 #endif //PROJECT_SERVO_H
--- Core/Src/servo.c
+++ Core/Src/servo.c
@@ -1,8 +1,13 @@
-//
-// Created by 1234455 on 2025/12/18.
-//
 #include "servo.h"
-void Servo_Set_angle()
-{
+#include "tim.h"
 
+void Servo_Init()           //舵机初始化
+{
+    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+}
+
+void Servo_Set_angle(const float angle)         //舵机角度设置
+{
+    uint32_t ccr_value = (uint32_t)(angle / 180.0 * 2000.0 + 500.0);
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, ccr_value);
 }
